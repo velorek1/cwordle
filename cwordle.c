dbb848d9f1ce41e581cc39a8546612f473c063bf
Robert Führicht <Robert.Fuehricht@jku.at>
2022-02-28 00:28:52 +0100

    Use cmake to build cwordle

--- cwordle.c
+++ cwordle.c
@@ -19,12 +19,14 @@ Wordle for Terminal.
 #include <time.h>
 #include <wchar.h>
 
+#include "config.h"
+
 #define dummyWord "ERASE\0"
 #define MAX_TEXTBOX 6
 //words in the dictionary file should have 5 chars and a 0x0A at the end
 #define SEPARATOR 0x0A
-#define DICTIONARY "dict.txt"
-#define POSSIBLES "possible.txt"
+#define DICTIONARY DATA_DIR "/dict.txt"
+#define POSSIBLES DATA_DIR "/possible.txt"
 
 //UNICODE chars
 #define HOR_LINE 9472
@@ -126,7 +128,7 @@ void writeWord(int index,  char text[MAX_TEXTBOX]);
 void cleanArea();
 int findIndex(char c);
 //FILE
-int openFile(FILE ** fileHandler, char *fileName, char *mode);
+int openFile(FILE ** fileHandler, const char *fileName, const char *mode);
 long countWords(FILE * fileHandler);
 void getWordfromDictionary(FILE * fileHandler, char WORD[MAX_TEXTBOX]);
 int isWordinDictionary(FILE * fileHandler, char WORD[MAX_TEXTBOX]);
@@ -684,7 +686,7 @@ int i=0;
  gameLoop();
 }
 
-int openFile(FILE ** fileHandler, char *fileName, char *mode) {
+int openFile(FILE ** fileHandler, const char *fileName, const char *mode) {
   int     ok;
   *fileHandler = fopen(fileName, mode);
   //check whether buffer is assigned
