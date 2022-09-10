33e172f92d70cd737451dd7bda071f4ba94ae66b
Raúl Sanguiao <velorek1@gmail.com>
2022-09-10 18:52:03 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo -e "\e[93mC-Wordle Game - Coded By v3l0r3k\e[0m"
 echo
 echo [+] Attempting to compile game...
 echo gcc cw.c -Wall -Wextra -o cw
-gcc cwordle.c -Wall -Wextra -o cw
+gcc cw.c -Wall -Wextra -o cw
 echo
 echo -e "Run as \e[93m./cw\e[0m" 
 echo Enjoy it!
